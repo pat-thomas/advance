36bd1e7b052ebae386700b2b26bee0f6e0832dd5
Pat Thomas <patthomassoftware@gmail.com>
2014-04-15 21:06:01 -0400

    Begin basic queue concept.

--- src/advance/core.clj
+++ src/advance/core.clj
@@ -1,6 +1,30 @@
 (ns advance.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmacro defqueue
+  [queue-name]
+  `(def ~queue-name (atom {:call-history []         
+                           :transformation-queue []}
+                          :meta {:is-queue true})))
+
+(defn queue?
+  [thing]
+  (= (:is-queue (meta thing)) true))
+
+(defn enqueue!
+  [queue function]
+  (if-not (queue? queue)
+    (throw (java.lang.Exception. (format "%s is not a queue." queue)))
+    (swap! queue update-in [:transformation-queue] conj function)))
+
+(defn dequeue!
+  [queue target]
+  (if-not (queue? queue)
+    (throw (java.lang.Exception. (format "%s is not a queue." queue)))
+    (if-let [function (first (:transformation-queue @queue))]
+      (let [result (function target)]
+        (do (swap! queue update-in [:transformation-queue] #(drop 1 %))
+            (swap! queue update-in [:call-history] conj result)
+            result))
+      (do (swap! queue assoc-in [:transformation-queue] [])
+          (swap! queue assoc-in [:call-history] [])
+          target))))
